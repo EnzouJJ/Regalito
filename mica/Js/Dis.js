3b3f2acdb684dcb168e3d77f6e483f2fb2dac5b6
EnzouJJ <enzoujatip@gmail.com>
2024-08-19 01:59:05 -0300

    Add files via upload

---
+++ mica/Js/Dis.js
@@ -0,0 +1,19 @@
+document.getElementById("BVer").addEventListener('click', function() {
+    document.getElementById("resultado").style.display = "block";
+})
+
+document.getElementById("BotonCerrar").addEventListener('click', function() {
+    document.getElementById("resultado").style.display = "none";
+    document.querySelector(".Contenedor-Binicio").style.display = "none";
+    document.querySelector(".Con-2").style.display = "block";
+    document.getElementById("gunter").style.display="none";
+
+})
+document.getElementById("B12").addEventListener('click',function(){
+    document.getElementById("cobijita").style.display="block";
+
+})
+document.getElementById("B1").addEventListener('click',function(){
+    document.getElementById("viejo").style.display="block";
+
+})
